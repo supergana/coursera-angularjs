4cf93969fe959ee5e61cd908f3e28c6762d589c8
supergana <supergana@gmail.com>
2020-05-03 03:12:17 +0530

    module 4 assignment

--- module4-solution/routes.js
+++ module4-solution/routes.js
@@ -7,7 +7,7 @@
   RoutesConfig.$inject = ['$stateProvider','$urlRouterProvider'];
   function RoutesConfig($stateProvider,$urlRouterProvider){
     // Redirect to tab 1 if no other URL matches
-    $urlRouterProvider.otherwise('/items/A');
+    $urlRouterProvider.otherwise('/');
 
     // Set up UI states
     $stateProvider
